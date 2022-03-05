828ef81e023472c71014d8fa89610dc44500275e
andrew <andrewacalrk248@gmail.com>
2022-03-05 15:36:21 -0500

    update build essential

--- nginx/metadata.rb
+++ nginx/metadata.rb
@@ -10,7 +10,7 @@ recipe 'nginx::source', 'Installs nginx from source and sets up configuration wi
 
 depends 'apt',             '7.0.0'
 depends 'bluepill',        '~> 2.3'
-depends 'build-essential', '~> 1.4'
+depends 'build-essential', '8.2.1'
 depends 'ohai',            '5.2.5'
 depends 'runit',           '~> 1.2'
 depends 'yum',             '~> 3.0'
