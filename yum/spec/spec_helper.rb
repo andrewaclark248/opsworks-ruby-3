e92187e85d8749bb2d834a3ff3bcec7c03b610db
andrew <andrewacalrk248@gmail.com>
2022-03-06 10:18:15 -0500

    update yum vesion

--- yum/spec/spec_helper.rb
+++ yum/spec/spec_helper.rb
@@ -1,10 +1,4 @@
 require 'chefspec'
 require 'chefspec/berkshelf'
 
-RSpec.configure do |config|
-  config.color = true               # Use color in STDOUT
-  config.formatter = :documentation # Use the specified formatter
-  config.log_level = :error         # Avoid deprecation notice SPAM
-  config.platform = 'centos'
-  config.version = '8'
-end
+at_exit { ChefSpec::Coverage.report! }
